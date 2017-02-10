4fcb6b548ded1f9e380c9b88c947d73a8e607419
Artem Novichkov <artem.novichkov@rosberry.com>
2017-02-10 09:40:48 +0600

    Change project structure and add scripts

---
+++ Template/Classes/AppDelegate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Template
+//
+//  Created by Artem Novichkov on 10/02/2017.
+//  Copyright © 2017 Rosberry. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
