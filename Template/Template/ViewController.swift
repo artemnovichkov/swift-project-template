38bde2d5f31ca1b3492a0e7752dd048fe0a1eea8
Artem Novichkov <artem.novichkov@rosberry.com>
2017-02-10 09:30:27 +0600

    Add generated project to make template

---
+++ Template/Template/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Template
+//
+//  Created by Artem Novichkov on 10/02/2017.
+//  Copyright © 2017 Rosberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
