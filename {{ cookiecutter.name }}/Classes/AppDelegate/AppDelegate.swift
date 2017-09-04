19e627c8a67be9e859cf80ac4ef59e8bdc68bea1
Artem Novichkov <artem.novichkov@rosberry.com>
2017-09-04 08:45:50 +0600

    Add saving of app version

--- {{ cookiecutter.name }}/Classes/AppDelegate/AppDelegate.swift
+++ {{ cookiecutter.name }}/Classes/AppDelegate/AppDelegate.swift
@@ -1,9 +1,5 @@
 //
-//  AppDelegate.swift
-//  {{ cookiecutter.name }}
-//
-//  Created by {{ cookiecutter.full_name }} on {% now 'local', '%d/%m/%Y' %}.
-//  Copyright © 2017 {{ cookiecutter.organization_name }}. All rights reserved.
+//  Copyright © {% now 'local', '%Y' %} {{ cookiecutter.organization_name }}. All rights reserved.
 //
 
 import UIKit
@@ -15,7 +11,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if let appInfo = Bundle.main.infoDictionary,
+            let shortVersionString = appInfo["CFBundleShortVersionString"] as? String,
+            let bundleVersion = appInfo["CFBundleVersion"] as? String {
+            let appVersion = "\(shortVersionString) (\(bundleVersion))"
+            UserDefaults.standard.appVersion = appVersion
+        }
         return true
+    }
+}
+
+fileprivate extension UserDefaults {
+    
+    var appVersion: String? {
+        get { return string(forKey: #function) }
+        set { set(newValue, forKey: #function) }
     }
 }
