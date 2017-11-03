a95e18c4d4551dfd0a78dd4b7219b3e872b7fba1
Artem Novichkov <artem.novichkov@rosberry.com>
2017-11-03 11:24:20 +0600

    Add optional downloading of Fabric frameworks

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,3 +7,8 @@ git add -A .
 git commit -m "Initial Commit"
 git flow init -d
 open -a Xcode {{ cookiecutter.name }}.xcodeproj
+{% if cookiecutter.fabric == "Yes" %}
+curl -O https://s3.amazonaws.com/kits-crashlytics-com/ios/com.twitter.crashlytics.ios/3.9.3/com.crashlytics.ios-manual.zip
+unzip com.crashlytics.ios-manual.zip
+rm com.crashlytics.ios-manual.zip
+{% endif %}
