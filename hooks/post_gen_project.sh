9f53359445bde619191155476fa9c22b6a7815ff
Artem Novichkov <artem.novichkov@rosberry.com>
2017-11-03 11:29:46 +0600

    Change order of commands for framework committing

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-git init
-git add -A .
-git commit -m "Initial Commit"
-git flow init -d
-open -a Xcode {{ cookiecutter.name }}.xcodeproj
 {% if cookiecutter.fabric == "Yes" %}
 curl -O https://s3.amazonaws.com/kits-crashlytics-com/ios/com.twitter.crashlytics.ios/3.9.3/com.crashlytics.ios-manual.zip
 unzip com.crashlytics.ios-manual.zip
 rm com.crashlytics.ios-manual.zip
 {% endif %}
+
+git init
+git add -A .
+git commit -m "Initial Commit"
+git flow init -d
+open -a Xcode {{ cookiecutter.name }}.xcodeproj
