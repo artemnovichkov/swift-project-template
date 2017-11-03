f2abbca87e68be4d7de15abfa2e1bf43d7b3340d
Artem Novichkov <artem.novichkov@rosberry.com>
2017-11-03 12:40:06 +0600

    Disable some outputs for hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,7 +4,7 @@ set -e
 
 {% if cookiecutter.fabric == "Yes" %}
 curl -O https://s3.amazonaws.com/kits-crashlytics-com/ios/com.twitter.crashlytics.ios/3.9.3/com.crashlytics.ios-manual.zip
-unzip com.crashlytics.ios-manual.zip
+unzip com.crashlytics.ios-manual.zip >/dev/null
 rm com.crashlytics.ios-manual.zip
 mkdir Frameworks
 mv Fabric.framework Frameworks/Fabric.framework
@@ -12,8 +12,8 @@ mv Crashlytics.framework Frameworks/Crashlytics.framework
 echo "✅ Fabric frameworks were added successfully. Don't forget to add API Key to Info.plist and API Key and Build Secret to Fabric Run Script Build Phase. The credentials are available here: https://www.fabric.io/settings/organizations"
 {% endif %}
 
-git init
-git add -A .
-git commit -m "Initial Commit"
-git flow init -d
+git init >/dev/null
+git add -A . >/dev/null
+git commit -m "Initial Commit" >/dev/null
+git flow init -d >/dev/null
 open -a Xcode {{ cookiecutter.name }}.xcodeproj
