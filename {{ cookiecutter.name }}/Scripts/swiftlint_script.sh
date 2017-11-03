7670a2f894766864fb741c992af12fbf35d34743
Artem Novichkov <artem.novichkov@rosberry.com>
2017-11-03 08:48:50 +0600

    Add Analyze configuration for linting
    Close #22

--- {{ cookiecutter.name }}/Scripts/swiftlint_script.sh
+++ {{ cookiecutter.name }}/Scripts/swiftlint_script.sh
@@ -1,6 +1,8 @@
-if which swiftlint >/dev/null; then
-  swiftlint
-else
-  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-  exit 1
+if [ "${CONFIGURATION}" = "Analyze" ]; then
+  if which swiftlint >/dev/null; then
+    swiftlint
+  else
+    echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+    exit 1
+  fi
 fi
