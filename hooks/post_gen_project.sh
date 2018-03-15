f956ffc45173df9d79fdf5983d6ef0892b06d080
Artem Novichkov <artem.novichkov@rosberry.com>
2018-03-15 12:38:41 +0600

    Add Swiftgen option

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -12,6 +12,11 @@ mv Crashlytics.framework Frameworks/Crashlytics.framework
 echo "✅ Fabric frameworks were added successfully. Don't forget to add API Key to Info.plist and API Key and Build Secret to Fabric Run Script Build Phase. The credentials are available here: https://www.fabric.io/settings/organizations"
 {% endif %}
 
+{% if cookiecutter.swiftgen == "Yes" %}
+echo "strings:\n  paths: Resources/en.lproj/Localizable.strings\n  templateName: structured-swift4\n  output: Resources/Swiftgen.swift" > swiftgen.yml
+swiftgen
+{% endif %}
+
 git init >/dev/null
 git add -A . >/dev/null
 git commit -m "Initial Commit" >/dev/null
