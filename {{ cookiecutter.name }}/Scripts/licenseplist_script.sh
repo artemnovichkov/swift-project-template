6cf862fa40f1172b1f20d827651a0da89ef817e1
Artem Novichkov <artem.novichkov@rosberry.com>
2018-07-20 12:57:40 +0600

    Add licenseplist_script.sh

---
+++ {{ cookiecutter.name }}/Scripts/licenseplist_script.sh
@@ -0,0 +1,8 @@
+if [ $CONFIGURATION = "Analyze" ]; then
+  if which license-plist >/dev/null; then
+    /usr/local/bin/license-plist --output-path $PRODUCT_NAME/Settings.bundle --suppress-opening-directory
+  else
+    echo "warning: LicensePlist not installed, download from https://github.com/mono0926/LicensePlist"
+    exit 1
+  fi
+fi
