7c72e4c72bdb11f39a5de43f64a737e1562aff7f
Artem Novichkov <novichkoff93@gmail.com>
2019-05-05 18:27:47 +0600

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -41,4 +41,4 @@ git init >/dev/null
 git add -A . >/dev/null
 git commit -m "Initial Commit" >/dev/null
 git flow init -d >/dev/null
-open -a /Applications/Xcode.app {{ cookiecutter.name }}.xcodeproj
+xed . >/dev/null
