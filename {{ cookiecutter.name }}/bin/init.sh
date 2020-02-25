71e3a695982583ab3da7a46363f2bcd5cc257287
Artem Novichkov <artem.novichkov@rosberry.com>
2020-02-25 09:59:29 +0600

    Update env with env flags

--- {{ cookiecutter.name }}/bin/init.sh
+++ {{ cookiecutter.name }}/bin/init.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
-#Download fastlane and update project name in .env
-fastfood update
-sed -i '' "s/project_name/$1/g" fastlane/.env
+uncomment_env_flag() {
+	sed -i '' "/$1/s/^# //g" fastlane/.env
+}
 
-#Initialize git
+uncomment_env_flags() {
+	flags=("$@")
+	for flag in "${flags[@]}";
+	do
+		uncomment_env_flag $flag	
+	done
+}
+
+# Download fastlane and update .env
+fastfood update
+sed -i '' "s|project_name|$1|g" fastlane/.env
+uncomment_env_flags GYM_SCHEME GYM_PROJECT CONFIGURATION_ADHOC CONFIGURATION_APPSTORE
+
+# Initialize git
 git init >/dev/null
 git add -A . >/dev/null
 git commit -m "Initial Commit" >/dev/null
 git flow init -d >/dev/null
 
-#Open Xcode
+# Open Xcode
 xed . >/dev/null
