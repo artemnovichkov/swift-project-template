246b53f52a5a7b564676dfb7b3a2c7c40cfb1163
Artem Novichkov <artem.novichkov@rosberry.com>
2020-02-25 10:17:24 +0600

    Remove carthage update

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,8 +3,7 @@
 set -e
 
 {% if cookiecutter.fabric == "Yes" %}
-carthage update --platform iOS
-echo "✅ Fabric frameworks were added successfully. Don't forget to add API Key to Info.plist and API Key and Build Secret to Fabric Run Script Build Phase. The credentials are available here: https://www.fabric.io/settings/organizations"
+echo "⚠️ Don't forget to add API Key to Info.plist and API Key and Build Secret to Fabric Run Script Build Phase. The credentials are available here: https://www.fabric.io/settings/organizations"
 {% endif %}
 
 {% if cookiecutter.swiftgen == "Yes" %}
